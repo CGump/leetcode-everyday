deb001e89fa5d3153482a422e62ded9bc7ba0784
cgump <824592076@qq.com>
2019-09-29 17:52:13 +0800

    整数反转

---
+++ 007_Easy_ReverseInteger.py
@@ -0,0 +1,20 @@
+class Solution:
+    '''
+    给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+    示例 1:   
+    输入: 123   
+    输出: 321
+
+    示例 2:   
+    输入: -123   
+    输出: -321
+    '''
+    def reverse(self, x: int) -> int:
+        num = str(x)[::-1].rstrip('-')
+        if int(num) < 2**31:
+            if x >= 0:
+                return int(num)
+            else:
+                return 0 - int(num)
+        return 0
